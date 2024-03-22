975f23e04d537a0ab39f7f68ae80aa5775726eb8
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-03-22 14:08:13 +1100

    Day 13
    Single on 22/03/2024

--- Assets/Scripts/EnemyTankShooting.cs
+++ Assets/Scripts/EnemyTankShooting.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class EnemyTankShooting : MonoBehaviour
@@ -7,17 +9,47 @@ public class EnemyTankShooting : MonoBehaviour
     public Rigidbody m_ShellPrefab;
     public Transform m_CannonTransform;
     public float m_LaunchForce = 30f;
-    
-    // Start is called before the first frame update
-    void Awake()
+    private bool m_CanShoot;
+    public float m_ShootDelay = 1f;
+    private float m_ShootTimer;
+
+    void Update()
     {
-        
+        if (m_CanShoot == true)
+        {
+            m_ShootTimer -= Time.deltaTime;
+            if (m_ShootTimer <= 0)
+            {
+                m_ShootTimer = m_ShootDelay;
+                Fire();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Fire()
     {
         Rigidbody shellInstance = Instantiate(m_ShellPrefab, m_CannonTransform.position, m_CannonTransform.rotation);
         shellInstance.velocity = m_LaunchForce * m_CannonTransform.forward;
+    }
+    
+    private void Awake()
+    {
+        m_CanShoot = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_CanShoot = true;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_CanShoot = false;
+        }
     }
 }
