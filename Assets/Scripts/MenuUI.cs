93a7a5072131daa82be1bb9890a7224b7b0b8f63
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-05-15 23:30:25 +1000

    Day 26'
    Completed out of class

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
-    public void GameStart(){}
+    public GameObject exitMenu;
+    
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log("Playing game!");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exited out of game!");
+        Application.Quit();
+    }
+
+    public void ExitMenu(bool exiting)
+    {
+        exitMenu.SetActive(exiting);
+    }
 }
